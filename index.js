b3e5ac66430e129f30a126ef4d5d28878077def1
Bingjie Gao <gilman.david04@gmail.com>
2016-11-07 13:34:16 +0000

    index databot

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-"use strict"
-var request = require("request-promise");
-var TdxApi = require("nqm-api-tdx");
-var debug = require("debug")("highGrab");
-var config = require("./config");
-var _ = require("lodash");
-var base64 = require("node-base64-image");
-var fs = require("fs");
-var Promise = require("bluebird");
-
-// var tdxAPI = new TdxApi(TDXconfig);
-// Promise.promisifyAll(tdxAPI);
-Promise.promisifyAll(base64);
-//
-//
-// tdxAPI.authenticate(config.shareId,config.shareKey,function(err,accessToken){
-//   if(err)
-//     throw err;
-//   else{
-//     insertData(tdxAPI,config);
-//   }
-// });
+/**
+ * GPS grab and store:
+ * @param {Object} tdx Api object.
+ * @param {Object} output functions.
+ * @param {Object} packageParams of the databot.
+ */
 
 function GrabHighway(tdxApi,packageParams){
   let options = {
@@ -92,3 +76,26 @@ function databot(input, output, context) {
         }
     });
 }
+
+"use strict"
+var request = require("request-promise");
+var TdxApi = require("nqm-api-tdx");
+var _ = require("lodash");
+var base64 = require("node-base64-image");
+var fs = require("fs");
+var Promise = require("bluebird");
+
+// var tdxAPI = new TdxApi(TDXconfig);
+// Promise.promisifyAll(tdxAPI);
+Promise.promisifyAll(base64);
+
+if (process.env.NODE_ENV == 'test') {
+    // Requires nqm-databot-trafficgrab.json file for testing
+    input = require('./databot-test.js')(process.argv[2]);
+} else {
+    // Load the nqm input module for receiving input from the process host.
+    input = require("nqm-databot-utils").input;
+}
+
+// Read any data passed from the process host. Specify we're expecting JSON data.
+input.pipe(databot);
